88e20a27b46bdf1bf8fc69685f38184defca6b4d
JUWON-KEVIN-LEE <quf9345@gmail.com>
2020-04-06 21:11:01 +0900

    feat: add firebase storage, firestore, and some android platform codes

--- app/src/main/java/lab/march/diary/ui/MainActivity.kt
+++ app/src/main/java/lab/march/diary/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package lab.march.diary.ui
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.net.Uri
@@ -7,9 +8,11 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.google.firebase.Timestamp
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.StorageReference
 import com.google.firebase.storage.ktx.storage
 import lab.march.diary.R
 import java.text.SimpleDateFormat
@@ -28,22 +31,27 @@ class MainActivity : AppCompatActivity() {
             "createdAt" to Timestamp(Date())
         )
 
-        firestore.collection("posts")
-            .add(post)
-            .addOnSuccessListener { documentReference ->
-                Log.d(TAG, "document snapshot written with ID: ${documentReference.id}")
-                Toast.makeText(this, "document snapshot written with ID: ${documentReference.id}", Toast.LENGTH_LONG).show()
+        val ref = firestore.collection("posts")
+            .document()
+
+        ref.set(post)
+            .addOnSuccessListener {
+                Log.d(TAG, "document snapshot written with ID: ${ref.id}")
+                Toast.makeText(this, "document snapshot written with ID: ${ref.id}", Toast.LENGTH_LONG).show()
             }
             .addOnFailureListener { exception ->
                 Log.d(TAG, "error writing document ${exception.message}")
                 Toast.makeText(this, "error writing document ${exception.message}", Toast.LENGTH_LONG).show()
             }
+            .addOnCanceledListener {
+                Log.d(TAG, "canceled writing document")
+                Toast.makeText(this, "canceled writing document", Toast.LENGTH_LONG).show()
+            }
     }
 
-    private fun pickImage() = startActivityForResult(
-        Intent(Intent.ACTION_PICK).apply { type = "image/*" },
-        REQUEST_CODE
-    )
+    private fun requestPermission() = ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+
+    private fun pickImage() = startActivityForResult(Intent(Intent.ACTION_PICK).apply { type = "image/*" }, REQUEST_CODE)
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -51,56 +59,46 @@ class MainActivity : AppCompatActivity() {
         if(requestCode == REQUEST_CODE) {
             if(resultCode == Activity.RESULT_OK) {
                 val uri = data?.data
-                uri?.let { uploadImage(it) }
-            } else {
 
-            }
-        }
-    }
+                uri?.let {
+                    val firebaseStorage = Firebase.storage
+                    val name = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.KOREA).format(Date()) + ".png"
 
-    private fun uploadImage(uri: Uri) {
-        val timestamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.KOREA).format(Date())
-        val name = timestamp.plus(".png")
-
-        val storage = Firebase.storage
-        val reference = storage.reference.child("images/$name")
-
-        reference.putFile(uri).addOnCompleteListener { task ->
-            if(task.isSuccessful) {
-                reference.downloadUrl.addOnSuccessListener {
-                    val firestore = Firebase.firestore
-                    firestore.collection("posts")
-                        .document()
-                        .set("data")
-                        .addOnCompleteListener { task ->
-                            if(task.isSuccessful) {
-
-                            } else {
-
-                            }
-                        }
+                    firebaseStorage.reference
+                        .child("images")
+                        .child(name)
+                        .uploadImage(it)
                 }
             } else {
-                task.exception?.let { /* handle exception */ }
+                /* fail to get uri */
             }
         }
-        reference.putFile(uri).addOnSuccessListener { snapshot ->
-
-                val timestamp = System.currentTimeMillis()
-                reference.downloadUrl
-                    .addOnSuccessListener {
-                        Firebase.firestore
-                            .collection("posts")
-                            .add("")
-                    }
-            }
-            .addOnFailureListener { exception ->
-
-            }
     }
 
-    private fun savePost(imageUri: Uri) {
+    private fun StorageReference.uploadImage(uri: Uri) = putFile(uri).addOnCompleteListener { task ->
+        when {
+            task.isSuccessful -> {
+                downloadUrl.addOnSuccessListener { uri ->
+                    val firestore = Firebase.firestore
 
+                    firestore.collection("posts")
+                        .document() /* auto-ID */
+                        .set(uri) /* data */
+                        .addOnCompleteListener {  }
+
+                }.addOnFailureListener { exception ->
+
+                }.addOnCanceledListener {
+
+                }
+            }
+            task.exception != null -> {
+
+            }
+            task.isCanceled -> {
+
+            }
+        }
     }
 
     companion object {
@@ -112,18 +110,18 @@ class MainActivity : AppCompatActivity() {
 
 /*
 <collection>
-    users
-        <document>
-        alovelace
-            <data>
-            first : ada
-            last : lovelace
-            born : 1895
-            </data>
-        aturing
-            first : ada
-            last : lovelace
-            born : 1895
-        </document>
+users
+    <document>
+    alovelace
+        <data>
+        first : ada
+        last : lovelace
+        born : 1895
+        </data>
+    aturing
+        first : ada
+        last : lovelace
+        born : 1895
+    </document>
 </collection>
 */
--- app/src/main/java/lab/march/diary/util/Extensions.kt
+++ app/src/main/java/lab/march/diary/util/Extensions.kt
@@ -1,2 +1,39 @@
 package lab.march.diary.util
 
+import android.transition.ChangeBounds
+import android.transition.TransitionManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.Interpolator
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import com.bumptech.glide.Glide
+
+fun View.visible() { visibility = View.VISIBLE }
+
+fun View.invisible() { visibility = View.INVISIBLE }
+
+fun View.gone() { visibility = View.GONE }
+
+val ViewGroup.layoutInflater: LayoutInflater get() = LayoutInflater.from(context)
+
+val View.layoutInflater: LayoutInflater get() = LayoutInflater.from(context)
+
+fun ConstraintLayout.update(constraintSet: ConstraintSet = ConstraintSet(), update: ConstraintSet.() -> Unit) {
+    constraintSet.clone(this)
+    constraintSet.update()
+    constraintSet.applyTo(this)
+}
+
+fun ConstraintLayout.delayedTransition(
+    duration: Long = 250,
+    interpolator: Interpolator = AccelerateDecelerateInterpolator()
+) = TransitionManager.beginDelayedTransition(this, ChangeBounds().also {
+    it.duration = duration
+    it.interpolator = interpolator
+})
+
+fun ImageView.loadImage(url: String) = Glide.with(this.context).load(url).into(this)
